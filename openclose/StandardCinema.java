ce2a68b52be8f82bc0605ee403928d6683cffc51
Taufik Setiawan <taufiksetiawan034@gmail.com>
2024-10-28 08:31:19 +0700

    cinema

---
+++ openclose/StandardCinema.java
@@ -0,0 +1,16 @@
+class StandardCinema extends Cinema {
+
+    MODUL IMPLEMENTASI PERANGKAT LUNAK
+    OLEH : Herdi Ashaury & Fajri Rakhmat Umbara
+    LABORATORIUM JURUSAN INFORMATIKA
+    FAKULTAS SAINS DAN INFORMATIKA UNIVERSITAS JENDERAL ACHMAD YANI
+    TERAKREDITASI “B” BAN-PT SKEPBAN-PT Nomor : 8304/SK/BAN-PT/Ak-PPJ/S/XII/2020
+    Jln. Trsn. Jend. Sudirman, Cimahi 401513 | Gd. Lab. II FSI Lt. 3 | Tlp. / Fax. (022) 6631 302
+    http://if.unjani.ac.id | email informatika. lab@if.unjani.ac.id
+    Halaman
+    36 / 93
+     public StandardCinema(double price) {
+     this.price = price;
+     }
+    } 
+    
